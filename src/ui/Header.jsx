66feaf721f0edde66ac80d02cff3e2f43b76524e
Dipesh Paudel <thedeepyash7@gmail.com>
2024-02-17 20:39:16 +0545

    order fetching with react router loader

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 export default function Header() {
   return (
     <div>
       <Link to="/">Nepali Fast Pizza</Link>
+      <SearchOrder />
+      <p>Dipesh Paudel</p>
     </div>
   );
 }
