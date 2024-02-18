a26d8a4bf2ab5696db1c3959f19952facf24c29c
Dipesh Paudel <thedeepyash7@gmail.com>
2024-02-18 13:56:11 +0545

    add css grid in the ui

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,14 +8,16 @@ export default function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
 
-      <main>
-        <h1>Content</h1>
-        <Outlet />
-      </main>
+      <div className="my-10 overflow-y-scroll">
+        <main className="mx-auto max-w-3xl">
+          <h1>Content</h1>
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
