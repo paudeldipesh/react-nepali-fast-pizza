d4a0ad4b9c18c3bf8eb30147b6a3ba65e449b383
Dipesh Paudel <thedeepyash7@gmail.com>
2024-02-18 10:06:38 +0545

    apply some tailwind css

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 export default function Header() {
   return (
-    <div>
-      <Link to="/">Nepali Fast Pizza</Link>
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase">
+      <Link to="/" className="tracking-widest">
+        Nepali Fast Pizza
+      </Link>
       <SearchOrder />
-      <p>Dipesh Paudel</p>
-    </div>
+      <Username />
+    </header>
   );
 }
