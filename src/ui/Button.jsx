f35d167c0835edbf1f2108439396da8ac745fd24
Dipesh Paudel <thedeepyash7@gmail.com>
2024-02-20 20:52:22 +0545

    update cart quantities

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -9,6 +9,7 @@ export default function Button({ children, disabled, to, type, onClick }) {
     secondary:
       "inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 active:bg-stone-300 disabled:cursor-not-allowed disabled:bg-stone-500 px-4 py-2.5 md:px-6 md:py-3.5",
     small: `${base} text-xs px-4 py-2 md:px-5 md:py-2.5`,
+    round: `${base} text-xs px-2.5 py-1 md:px-3.5 md:py-2 text-sm`,
   };
 
   if (to) {
