5bab62c51ae5994354c003a6caf0901e36b049af
Dipesh Paudel <thedeepyash7@gmail.com>
2024-02-20 11:45:36 +0545

    model user state with redux toolkit

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
+
 export default function Username() {
+  const { username } = useSelector((state) => state.user);
+
+  if (!username) return null;
+
   return (
-    <div className="hidden text-sm font-semibold md:block">Dipesh Paudel</div>
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
   );
 }
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,18 +1,18 @@
-function getPosition() {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-}
+import { createSlice } from "@reduxjs/toolkit";
 
-async function fetchAddress() {
-  const positionObj = await getPosition();
-  const position = {
-    latitude: positionObj.coords.latitude,
-    longitude: positionObj.coords.longitude,
-  };
+const initialState = {
+  username: "",
+};
 
-  const addressObj = await getAddress(position);
-  const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateName: (state, action) => {
+      state.username = action.payload;
+    },
+  },
+});
 
-  return { position, address };
-}
+export const { updateName } = userSlice.actions;
+export default userSlice.reducer;
